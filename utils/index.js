c8361c095df62040619758757f7c931718310b95
Denis Shiryaev/ <arokmeister@yandex.ru/>
2023-04-19 00:24:56 +0300

    fix: исправления после автотестов 2

--- utils/index.js
+++ utils/index.js
@@ -1,18 +1,23 @@
 const mongooseError = require('mongoose').Error;
 const {
-  BAD_REQUEST_400, SERVER_ERROR_500,
+  BAD_REQUEST_400, SERVER_ERROR_500, NOT_FOUND_404,
 } = require('../constants/constants');
 
 const handleError = (res, err, defMessage = 'Ошибка') => {
-  const resError = { message: `${defMessage}: ${Object.values(err.errors).map((error) => error.message).join(' ')}` };
-  if (err instanceof mongooseError.CastError || err instanceof mongooseError.ValidationError) {
+  const textError = { message: `${defMessage}: ${err.message}` };
+  if (err instanceof mongooseError.ValidationError) {
+    const resError = { message: `${defMessage}: ${Object.values(err.errors).map((error) => error.message).join(' ')}` };
     res.status(BAD_REQUEST_400).send(resError);
     return;
   }
+  if (err instanceof mongooseError.CastError) {
+    res.status(NOT_FOUND_404).send(textError);
+    return;
+  }
   if (err instanceof Error) {
-    res.status(err.statusCode).send(resError);
+    res.status(err.statusCode).send(textError);
   } else {
-    res.status(SERVER_ERROR_500).send(resError);
+    res.status(SERVER_ERROR_500).send(textError);
   }
 };
 

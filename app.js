c780f5c48a239b2ebed665b79508f366249df9ab
Denis Shiryaev/ <arokmeister@yandex.ru/>
2023-05-09 14:35:24 +0300

    feat: add winston

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const router = require('./routes');
 const { handleError } = require('./utils/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,7 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(handleError);

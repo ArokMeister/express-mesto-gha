497b790a9cc59f1c60260ade06c07825b56195cf
Denis Shiryaev/ <arokmeister@yandex.ru/>
2023-05-09 15:13:34 +0300

    fix remove dependec

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const router = require('./routes');
 const { handleError } = require('./utils/index');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -18,11 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(requestLogger);
-
 app.use(router);
 
-app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
 
